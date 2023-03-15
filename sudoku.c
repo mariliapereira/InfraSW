77e89ae14dab9727df49ef1a7dd0842e68ab8140
mariliapereira <msap@cesar.school>
2023-03-15 18:52:01 +0000

    fix validateSub

--- sudoku.c
+++ sudoku.c
@@ -2,13 +2,9 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define NUM_THREADS 13
-#define MAX(a,b) (((a)>(b))?(a):(b))
-
 void *validateLine(void *datastruct);
 void *validateCol(void *datastruct);
 void *validateSub(void *datastruct);
-void *validate(void *datastruct);
 
 typedef struct inputstruct
 {
@@ -25,13 +21,6 @@ typedef struct
     int *invalid;
 } datastruct;
 
-typedef struct
-{
-    input *info;
-    int *invalid;
-    int l, r;
-} validation;
-
 int main(int argc, char *argv[])
 {
     int n, m, a, b;
@@ -42,7 +31,7 @@ int main(int argc, char *argv[])
     fOut = fopen("sudoku_msap.out", "w");
     if (fp == NULL)
     {
-        fprintf(fOut, "Error to read file");
+        printf("Error to read file");
         return -1;
     }
 
@@ -52,7 +41,7 @@ int main(int argc, char *argv[])
 
     if (n != m)
     {
-        fprintf(fOut, "Invalid number of parameters");
+        printf("Invalid number of parameters");
         return -1;
     }
 
@@ -60,7 +49,7 @@ int main(int argc, char *argv[])
 
     if (a * b != m)
     {
-        fprintf(fOut, "Invalid number of parameters");
+        printf("Invalid number of parameters");
         return -1;
     }
 
@@ -79,17 +68,30 @@ int main(int argc, char *argv[])
     }
     int invalid = 0;
 
-    pthread_t threads[NUM_THREADS];
+    pthread_t threads[3 * n];
 
-    int group_size = (3 * n) / NUM_THREADS;
-    for (int i = 0; i < NUM_THREADS; i++)
+    datastruct data[n];
+
+    for (int i = 0; i < n; i++)
     {
-        validation vd;
-        vd.info = &info;
-        vd.l = i * group_size;
-        vd.r = MAX((i + 1) * group_size, 3 * n );
-        vd.invalid = &invalid;
-        int err = pthread_create(&threads[i], NULL, (void *)validate, (void *)&vd);
+        data[i].info = &info;
+        data[i].i = i;
+        data[i].invalid = &invalid;
+        int err;
+
+        err = pthread_create(&threads[i * 3], NULL, (void *)validateCol, (void *)&data[i]);
+        if (err)
+        {
+            fprintf(stderr, "Error - pthread_create() return code: %d\n", err);
+            exit(EXIT_FAILURE);
+        }
+        err = pthread_create(&threads[i * 3 + 1], NULL, (void *)validateLine, (void *)&data[i]);
+        if (err)
+        {
+            fprintf(stderr, "Error - pthread_create() return code: %d\n", err);
+            exit(EXIT_FAILURE);
+        }
+        err = pthread_create(&threads[i * 3 + 2], NULL, (void *)validateSub, (void *)&data[i]);
         if (err)
         {
             fprintf(stderr, "Error - pthread_create() return code: %d\n", err);
@@ -97,7 +99,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    for (int i = 0; i < NUM_THREADS; i++)
+    for (int i = 0; i < 3 * n; i++)
     {
         pthread_join(threads[i], NULL);
     }
@@ -108,33 +110,6 @@ int main(int argc, char *argv[])
     }
     fprintf(fOut, "SUCCESS");
     return 0;
-}
-
-void *validate(void *ptr)
-{
-    validation *vd = (validation *)ptr;
-
-    for (int i = vd->l; i < vd->r; i++)
-    {
-        datastruct data;
-        data.info = vd->info;
-        data.i = i / 3;
-        data.invalid = vd->invalid;
-        int err;
-
-        if (i % 3 == 0)
-        {
-            validateCol((void *)&data);
-        }
-        if (i % 3 == 1)
-        {
-            validateLine((void *)&data);
-        }
-        if (i % 3 == 2)
-        {
-            validateSub((void *)&data);
-        }
-    }
 }
 
 void *validateLine(void *vd)
@@ -201,9 +176,9 @@ void *validateSub(void *vd)
     ds = (datastruct *)vd;
     int mkd[ds->info->size];
     int k = ds->i;
-    int groupI = k / ds->info->b;
-    int groupJ = k % ds->info->b;
-    int startI = groupI * ds->info->a; // ponto de partida do grupo
+    int groupI = k % ds->info->b;  //em que subgrade eu to 
+    int groupJ = k / ds->info->b;
+    int startI = groupI * ds->info->a; //posição do começo do grupo
     int startJ = groupJ * ds->info->b;
 
     for (int i = 0; i < ds->info->size; i++)
@@ -212,8 +187,10 @@ void *validateSub(void *vd)
     }
     for (int z = 0; z < ds->info->size; z++)
     {
-        int i = startI + z % ds->info->a;
-        int j = startJ + z / ds->info->b;
+        int dI = z % ds->info->a; //agora é elementos do subgrupo 
+        int dJ = z / ds->info->a; 
+        int i = startI + dI;
+        int j = startJ + dJ;
         if (ds->info->matrix[i][j] < 1 || ds->info->matrix[i][j] > ds->info->size)
         {
             *ds->invalid = 1;
