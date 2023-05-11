30f2f9ea34b8925a837540d8d2a3f8f5149cfa85
mariliapereira <msap@cesar.school>
2023-05-11 00:14:17 +0000

    //prints

--- Metrorec/metrorec.c
+++ Metrorec/metrorec.c
@@ -26,18 +26,18 @@ void estacao_preecher_vagao(struct estacao * station, int assentos) {
         pthread_cond_wait(&station->station_empty, &station->mutex);
     }   
     station->free_spots = assentos; 
-    printf("Car waiting with %d free spots\n", station->free_spots);
+    //printf("Car waiting with %d free spots\n", station->free_spots);
     pthread_cond_broadcast(&station->car_ready);
     pthread_cond_wait(&station->ready_to_leave, &station->mutex);
     pthread_cond_signal(&station->station_empty);
-    printf("Car leaving with %d free spots\n", station->free_spots);
+    //printf("Car leaving with %d free spots\n", station->free_spots);
     pthread_mutex_unlock(&station->mutex); 
 }
 
 void estacao_espera_pelo_vagao(struct estacao * station) {
     pthread_mutex_lock(&station->mutex);   
     station->passengers++; 
-    printf("%d passengers waiting\n", station->passengers);
+    //printf("%d passengers waiting\n", station->passengers);
     while (station->free_spots == station->embarking) {
         pthread_cond_wait(&station->car_ready, &station->mutex);
     }
@@ -57,5 +57,6 @@ void estacao_embarque(struct estacao * station) {
     if(station->free_spots == 0 || (station->passengers == 0 && station->embarking == 0)) {
         pthread_cond_signal(&station->ready_to_leave);
     }
+    //printf("Embarque realizado\n");
     pthread_mutex_unlock(&station->mutex); 
 }
