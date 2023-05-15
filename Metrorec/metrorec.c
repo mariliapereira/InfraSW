f4dd963c2d02ae1b6bf37902d144655a9d0f7b86
mariliapereira <msap@cesar.school>
2023-05-15 11:54:24 +0000

    final version

--- Metrorec/metrorec.c
+++ Metrorec/metrorec.c
@@ -4,7 +4,8 @@ struct estacao {
     pthread_cond_t car_ready;    
     pthread_cond_t ready_to_embark;   
     pthread_cond_t ready_to_leave;
-    pthread_cond_t station_empty;
+    pthread_cond_t car_left;
+    pthread_cond_t station_empty; 
     pthread_mutex_t mutex;
     int free_spots;
     int passengers;
@@ -19,21 +20,23 @@ void estacao_init(struct estacao *station) {
     pthread_cond_init(&station->car_ready, NULL);     
     pthread_cond_init(&station->ready_to_embark, NULL); 
     pthread_cond_init(&station->ready_to_leave, NULL);
+    pthread_cond_init(&station->car_left, NULL);
     pthread_cond_init(&station->station_empty, NULL);
 }
 
 void estacao_preencher_vagao(struct estacao * station, int assentos) {
     pthread_mutex_lock(&station->mutex);
-    while(station->free_spots > 0) {
+    while(station->free_spots > 0) {  //dispensável porque no pdf diz para "assumir que nunca haverá mais que um carro na estação por vez", mas como já tinha feito, deixei
         //printf("another car waiting with %d free spots\n", station->free_spots);
         pthread_cond_wait(&station->station_empty, &station->mutex);
-    }   
+    }  
     station->free_spots = assentos; 
     //printf("Car waiting with %d free spots\n", station->free_spots);
     pthread_cond_broadcast(&station->car_ready);
     pthread_cond_wait(&station->ready_to_leave, &station->mutex);
-    pthread_cond_signal(&station->station_empty);
     //printf("Car leaving with %d free spots\n", station->free_spots);
+    pthread_cond_signal(&station->car_left);
+    pthread_cond_signal(&station->station_empty);
     pthread_mutex_unlock(&station->mutex); 
 }
 
@@ -57,9 +60,10 @@ void estacao_embarque(struct estacao * station) {
     }
     station->embarking--;    
     station->free_spots--;
+    //printf("Embarque realizado\n");
     if(station->free_spots == 0 || (station->passengers == 0 && station->embarking == 0)) {
         pthread_cond_signal(&station->ready_to_leave);
+        pthread_cond_wait(&station->car_left, &station->mutex);
     }
-    //printf("Embarque realizado\n");
     pthread_mutex_unlock(&station->mutex); 
 }
